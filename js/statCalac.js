10832023e4fb6f9d0d64fd26ebf4e209d661cb2e
karabako <48029405+karabako@users.noreply.github.com>
2020-01-02 17:16:15 +0900

    Update statCalac.js

--- js/statCalac.js
+++ js/statCalac.js
@@ -4,7 +4,8 @@ const base = {
   "ギャラドス": [95, 125, 79, 60, 100, 81]
 }
 function statCalc(base) {
-  const stat, level = 100;
+  const stat;
+  const level = 100;
   for (i = 0; i < 32; i++) {
     //hp
     stat[i][0] = ((base[0] * 2 + i) * level / 100) + level + 10;
