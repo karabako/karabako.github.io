86b62df6154c16118310d589d27f9183126d7719
karabako <48029405+karabako@users.noreply.github.com>
2020-01-02 17:12:14 +0900

    test

--- js/event.js
+++ js/event.js
@@ -1,8 +1,8 @@
 document.onkeydown = keydown;
 
 
-function keydown(event) {
-  var code = event.keyCode;
+function keydown(e) {
+  var code = e.keyCode;
   // console.log(code);
   if (code == 13) {
     document.getElementsByTagName("button")[0].click();
@@ -10,10 +10,21 @@ function keydown(event) {
 }
 
 
-function whl(event) {
-  event.preventDefault();
-	event.target.value=Number(event.target.value)-event.deltaY/100;
-
-
+function whl(e) {
+  e.preDefault();
+	e.target.value=Number(e.target.value)-e.deltaY/100;
 }
 document.getElementById("statA").onwheel=whl;
+
+function changeP(e){
+  console.log(base[e.target.value])
+  const stat=statCalc(base[e.target.value]);
+  document.getElementById("statH")=stat[0][31];
+  document.getElementById("statA")=stat[1][31];
+  document.getElementById("statB")=stat[2][31];
+  document.getElementById("statC")=stat[3][31];
+  document.getElementById("statD")=stat[4][31];
+  document.getElementById("statS")=stat[5][31];
+}
+document.getElementById("poke").onchange=changeP;
+console.log(base[e.target.value])
